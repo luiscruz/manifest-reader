ee9f00a3ba948ea4f77ea4223ae80af3455a4faa
Luis Cruz <ei06034@fe.up.pt>
2016-10-27 16:39:56 +0100

    added versionName support

--- manifest_reader.py
+++ manifest_reader.py
@@ -4,6 +4,7 @@ import xml.etree.ElementTree
 @click.command()
 @click.option('--android-sdk-version', 'attribute', flag_value='android-sdk-version', help='Retrieve Android SDK version.')
 @click.option('--package-name', 'attribute', flag_value='package-name', help='Retrieve app\'s package name.')
+@click.option('--version-code', 'attribute', flag_value='version-code', help='Retrieve app\'s version name.')
 @click.argument('manifest')
 def tool(attribute, manifest):
     """Tool to read Android manifests."""
@@ -12,3 +13,6 @@ def tool(attribute, manifest):
         click.echo(manifest_node.get('package'))
     elif 'android-sdk-version' == attribute:
         click.echo(manifest_node.get('platformBuildVersionCode'))
+    elif 'version-name' == attribute:
+        click.echo(manifest_node.get('android:versionName'))
+        
